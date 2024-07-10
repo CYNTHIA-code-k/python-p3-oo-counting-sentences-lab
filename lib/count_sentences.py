444c617368a7c25acc4c3c947e279cd7fab5e90a
cynthia <cyeeyearn@gmail.com>
2024-07-10 22:18:18 +0300

    sentence files

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
+import re  # Add this line at the top of your file
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        # Replace ellipsis with a single period to avoid counting it as multiple sentences
+        text = self.value.replace('...', '.')
+        # Use regex to split the text into sentences
+        sentences = re.split(r'[.!?]+', text)
+        # Remove empty strings from the list
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
+
